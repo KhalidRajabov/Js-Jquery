b560e827b5ebe91fcf1c1e7b621a74b6e2515e03
Khalid Radjabov <rammkhalid@gmail.com>
2022-06-01 18:28:23 +0400

    works but no check enabled\

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,39 @@
 
 $(document).ready(function () {
 
+    let loginchecker = JSON.parse(localStorage.getItem("dbUser"))
+
+    if (loginchecker!=null) {
+        
+        if (loginchecker.login==true) {
+            
+            alert("You're already logged in\nRedirecting to new page")
+            window.location="login.html"
+        }
+        if (loginchecker.login==false) {
+            
+            alert("Please log in")
+            
+            let user = JSON.parse(localStorage.getItem("dbUser"))
+
+            $("#btnLogin").click(function () {
+                if ($("#username").val() == user.username && $("#password").val() == user.password) {
+        
+                    user.login = true
+        
+        
+                    localStorage.setItem("dbUser", JSON.stringify(user))
+        
+                    window.location = "login.html"
+                }
+                else {
+                    alert("wrong")
+                }
+            })
+        }
+    }
     
-    
+    if (loginchecker==null) {
         let user = {
             username: "asdf",
             password: 1234,
@@ -10,19 +41,20 @@ $(document).ready(function () {
         }
         $("#btnLogin").click(function () {
             if ($("#username").val() == user.username && $("#password").val() == user.password) {
-
+    
                 user.login = true
-
-
+    
+    
                 localStorage.setItem("dbUser", JSON.stringify(user))
-
+    
                 window.location = "login.html"
             }
             else {
                 alert("wrong")
             }
         })
-    
+    }
+
 
 
 
